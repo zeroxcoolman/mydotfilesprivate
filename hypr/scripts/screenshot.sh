77a3e496f4a78db710c513a9715a2074aa8b8c62
i thought yea <107873399+zeroxcoolman@users.noreply.github.com>
2025-12-02 18:55:30 +0000

    Enhance screenshot.sh to save and copy screenshots
    Updated screenshot script to save and copy to clipboard.

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -1,2 +1,9 @@
-!#/bin/zsh
-grim -g "$(slurp)" &
+#!/bin/zsh
+
+file=~/Pictures/screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
+
+# Take screenshot once and save it
+grim -g "$(slurp)" "$file"
+
+# Copy that same file to clipboard
+wl-copy < "$file"
