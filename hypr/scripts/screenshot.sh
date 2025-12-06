9960beb59f4d07ffa7e72b7f88c923556d26bdcb
tung tung tung <ehsanislam467@gmail.com>
2025-12-06 17:41:05 +0000

    swappy -> satty

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -3,7 +3,7 @@
 file=~/Pictures/screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
 
 # Take screenshot of selected region and send to swappy
-grim -g "$(slurp)" - | swappy -f - -o "$file"
+grim -g "$(slurp)" - | satty -f - -o "$file"
 
 # Copy final (edited) screenshot to clipboard
 wl-copy < "$file"
