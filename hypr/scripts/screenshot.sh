90d366df8b70d4835105995e83e2f3dfea8d3632
tung tung tung <ehsanislam467@gmail.com>
2025-12-07 11:03:07 +0000

    updated screenshot.sh to work with satty/config.toml

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -1,10 +1,5 @@
 #!/bin/zsh
 
-file=~/Pictures/screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
-
-# Take screenshot of selected region and send to swappy
-grim -g "$(slurp)" - | satty -f - -o "$file"
-
-# Copy final (edited) screenshot to clipboard
-wl-copy < "$file"
+# Selected screenshot passed to Satty, let Satty handle saving/copying
+grim -g "$(slurp)" - | satty -f -
 
