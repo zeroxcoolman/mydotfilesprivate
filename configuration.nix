c4553e98b2062f1d12a7d2aa7abf7139fe8755f6
i thought yea <107873399+zeroxcoolman@users.noreply.github.com>
2025-12-13 21:01:07 +0000

    Switch boot loader from systemd-boot to GRUB

--- configuration.nix
+++ configuration.nix
@@ -14,8 +14,11 @@
   nixpkgs.config.allowUnfree = true;  
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.useOSProber = true;
 
   networking.hostName = "crawlerE30"; # Define your hostname.
 
