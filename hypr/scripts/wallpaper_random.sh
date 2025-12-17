650b953da2a1031dfdbb67b2fa97a78a00ca07a8
i thought yea <107873399+zeroxcoolman@users.noreply.github.com>
2025-12-17 18:04:14 +0000

    Enhance wallpaper_random.sh with symlink feature
    Add symlink creation for the current wallpaper.

--- hypr/scripts/wallpaper_random.sh
+++ hypr/scripts/wallpaper_random.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+symlink_path="$HOME/.config/hypr/current_wallpaper"
 wallpapers_dir="$HOME/Pictures/wallpapers"
 
 random_wallpaper=$(find "$wallpapers_dir" -maxdepth 1 -type f | shuf -n 1)
 
+## set wallpaper
+
 matugen image $random_wallpaper
+
+## create symlink
+mkdir -p "$(dirname "$symlink_path")"
+ln -sf "$random_wallpaper" "$symlink_path"
