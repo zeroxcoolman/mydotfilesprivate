2e8a5deaff3a0e7558c9f33c29e6bc439d8ca787
i thought yea <107873399+zeroxcoolman@users.noreply.github.com>
2025-12-18 08:58:32 +0000

    Create oxwm-colors.lua

---
+++ matugen/templates/oxwm-colors.lua
@@ -0,0 +1,11 @@
+return {
+    bg = "#{{colors.surface.default.hex_stripped}}",
+    fg = "#{{colors.on_surface.default.hex_stripped}}",
+    red = "#{{colors.error.default.hex_stripped}}",
+    green = "#{{colors.tertiary.default.hex_stripped}}",
+    cyan = "#{{colors.primary.default.hex_stripped}}",
+    light_blue = "#{{colors.secondary.default.hex_stripped}}",
+    purple = "#{{colors.primary_container.default.hex_stripped}}",
+    grey = "#{{colors.outline.default.hex_stripped}}",
+    sep = "#{{colors.outline_variant.default.hex_stripped}}",
+}
