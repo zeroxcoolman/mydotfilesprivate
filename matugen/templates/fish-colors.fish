979e027bb30114c7b3477d2190e10b629c8825e2
wryyyy <107873399+zeroxcoolman@users.noreply.github.com>
2025-12-20 13:08:35 +0000

    changes

---
+++ matugen/templates/fish-colors.fish
@@ -0,0 +1,12 @@
+# Auto-generated
+# by
+# Matugen
+
+set -g MAT_BG        "#{{colors.surface.default.hex_stripped}}"
+set -g MAT_SURFACE   "#{{colors.on_surface.default.hex_stripped}}"
+set -g MAT_PRIMARY   "#{{colors.primary.default.hex_stripped}}"
+set -g MAT_SECONDARY "#{{colors.secondary.default.hex_stripped}}"
+set -g MAT_TERTIARY  "#{{colors.tertiary.default.hex_stripped}}"
+set -g MAT_ERROR      "#{{colors.error.default.hex_stripped}}"
+set -g MAT_OUTLINE    "#{{colors.outline.default.hex_stripped}}"
+set -g MAT_ON_SURF    "#{{colors.on_surface.default.hex_stripped}}"
