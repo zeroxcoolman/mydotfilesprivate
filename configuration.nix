c01c5d1c00863fc9dcdb56907a3d51dbd1fcd146
i thought yea <107873399+zeroxcoolman@users.noreply.github.com>
2026-02-22 16:25:05 +0000

    Refactor NixOS configuration with new settings

--- configuration.nix
+++ configuration.nix
@@ -2,77 +2,43 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
-
+{ config, lib, pkgs, dwm-src, ... }:
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-  
-  # allow unfree packages
-  nixpkgs.config.allowUnfree = true;  
 
-  # Use the systemd-boot EFI boot loader.
+  # Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # fuck nvidia
+  nixpkgs.config.allowUnfree = true;
+
+  # Use the GRUB EFI boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.device = "nodev";
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "crawlerE30"; # Define your hostname.
+  boot.kernelPackages = pkgs.linuxPackages;
+
+  networking.hostName = "crawlere30"; # Define your hostname.
 
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
   
   # Turn bluetooth on.
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
-  services.blueman.enable = true;  
+  services.blueman.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/London";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-
-  # Enable hyprland.
-  programs.hyprland.enable = true;
-  programs.hyprlock.enable = true;
-  services.hypridle.enable = true;
-  
-  # Enable fish shell.
-  programs.fish.enable = true;  
-  
-  # starship shell
-  programs.starship.enable = true;
- 
-  # flatpak
-  services.flatpak.enable = true;
-
-  # NVIDIA DRIVERS HYBRID
-  
-  # Enable OpenGL
-  hardware.opengl = {
-	enable = true;
-  };
-
-  # Load NVIDIA drivers
+  # ass nvidia stuff
   services.xserver.videoDrivers = ["nvidia"];
   
   hardware.nvidia = {
@@ -92,134 +58,112 @@
 	};
   };
 
-  # Configure keymap in X11
+	nixpkgs.overlays = [
+	 (final: prev: {
+	   dwm = prev.dwm.overrideAttrs (old: {
+	     src = dwm-src;
+	     buildInputs = (old.buildInputs or []) ++ [ final.imlib2 ];
+	   });
+	 })
+	];
+
+  services.picom.enable = true;
+
+  programs.fish.enable = true;
+  services.flatpak.enable = true;
+  hardware.opengl = {
+    enable = true;
+    };
+
+  services.xserver = {
+    enable = true;
+    autoRepeatDelay = 200;
+    autoRepeatInterval = 35;
+    displayManager.sessionCommands = ''
+      xwallpaper --zoom ~/.config/hypr/current_wallpaper 
+      '';
+    windowManager = {
+      xmonad = {
+        enable = true;
+        enableContribAndExtras = true;
+      };
+    };
+  };
+    
+ services.xserver.windowManager.session = [
+   {
+     name = "dwm";
+     start = ''
+       exec /home/ehsab/.local/bin/start-dwm
+     '';
+   }
+ ];
+
+
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;  
+
   services.xserver.xkb.layout = "gb";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
   services.pipewire = {
-     enable = true;
-     pulse.enable = true;
+    enable = true;
+    pulse.enable = true;
   };
 
-  # gnome-keyring
-  services.gnome.gnome-keyring.enable = true;  
+   services.libinput.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.ehsab = {
+   users.users.ehsab = {
      isNormalUser = true;
+     shell = pkgs.fish;
      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
      packages = with pkgs; [
        tree
      ];
+   };
+
+  programs.firefox.enable = true;
+
+  xdg.portal = {
+	enable = true;
+	extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
   };
 
-  # programs.firefox.enable = true;
-  # programs.chromium.enable = true;   
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
-     kitty
-     git
-     neovim
-     mesa-demos
-     cava
-     fastfetch
-     fish
-     cargo
-     nwg-dock-hyprland
-     rofi
-     satty
-     swaynotificationcenter
-     waybar
-     brightnessctl
-     swww
-     nerd-fonts.jetbrains-mono
-     xdotool
-     wtype
-     wf-recorder
-     wl-clipboard
-     pavucontrol
-     pamixer
-     cliphist
-     eza
-     wlogout
-     matugen
-     jq
-     starship
-     slurp
-     grim
-     networkmanagerapplet
-     chromium
-     yad
-     discord
-     gnome-keyring
-     meson
-     ninja
-     gcc
-     pkg-config
-     glib
-     cmake
-     pokemon-colorscripts
-     ani-cli
-     imagemagick
-     feh
-     libnotify
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+    matugen
+    zoxide
+    xmobar
+    haskell-language-server
+    gnumake
+    kitty
+    fish
+    cargo
+    cava
+    rofi
+    satty
+    brightnessctl
+    wl-clipboard
+    wtype
+    xdotool
+    nerd-fonts.jetbrains-mono
+    btop
+    dwm
+    acpi
+    xorg.xrdb
+    discord
+    pokemon-colorscripts
+    lsd
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  fonts = {
+	enableDefaultPackages = true;
+	packages = with pkgs; [
+		nerd-fonts.jetbrains-mono
+	];
+  };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.11"; # Did you read the comment?
-
-}
-
+ system.stateVersion = "25.11"; # Did you read the comment?
